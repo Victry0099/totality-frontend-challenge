e6f51e3b5801f4ece25e3083c9f220814e1d7f21
Victry0099 <prakashkush0099@gmail.com>
2024-10-14 12:18:58 +0530

    new file:   public/images/rental-hero.jpg 	modified:   src/components/HeroPage.jsx

--- src/components/HeroPage.jsx
+++ src/components/HeroPage.jsx
@@ -3,7 +3,7 @@ import React, { memo } from "react";
 const HeroPage = memo(() => {
   return (
     <main className="px-1  md:px-4  pb-4 lg:px-8 lg:pb-8 rounded-2xl">
-      <section className="bg-[url('images/heropage.png')] bg-fixed relative w-full h-[50vh] md:h-[100vh] sm:h-[70vh] bg-center bg-no-repeat md:bg-cover z-0 rounded-5xl">
+      <section className="bg-[url('images/rental-hero.jpg')] bg-fixed relative w-full h-[50vh] md:h-[100vh] sm:h-[70vh] bg-center bg-no-repeat md:bg-cover z-0 rounded-5xl">
         <div className="absolute inset-0 bg-gradient-to-b from-[rgba(26,26,26,0.8)] via-[rgba(26,26,26,0.1)] to-[rgba(26,26,26,0)] z-10 rounded-2xl"></div>
 
         <div className="flex justify-center items-center z-20 w-full h-full">
